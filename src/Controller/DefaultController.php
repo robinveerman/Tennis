629e4f738ba24d0b1def77a0f3156bbfceaf461d
Robin <robinveerman71@gmail.com>
2019-03-01 12:19:52 +0100

    Meerdere rondes maken

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -64,19 +64,12 @@ class DefaultController extends AbstractController {
 		$em      = $this->getDoctrine()->getManager();
 		$spelers = $em->getRepository( Speler::class )->findAll();
 
+		// alle spelers
 		$wedstrijd = [];
 		foreach ( $spelers as $speler ) {
 			array_push( $wedstrijd, $speler->getId() );
 		}
 
-
-		/* maximaal 128 deelnemers, formule voor rest is    ( er zijn b.v. 100 deelnemers)
-		 * secuence is 128, 64, 32, 16, 8, 4, 2 ronde 1, 2, 3, 4, 5, 6, 7
-		 * formule 1:  aantal_deelnemers - (128 - aantal_deelnemers) = deelnemers_ronde_1    (72) (36 wedstrijden)
-		 * er gaan ;  naar_ronde_2 = aantal_deelnemers - deelnemers_ronde_1     (28 gaan er zo door).
-		 *
-		 * als er teveel aanmeldingen zijn dan alle spelers boven 128 uitsluiten.
-		 */
 		if ( shuffle( $wedstrijd ) ) {
 			if ( count( $wedstrijd ) == 128 ) {
 
@@ -91,35 +84,11 @@ class DefaultController extends AbstractController {
 					$em->flush();
 				}
 
-			} elseif ( count( $wedstrijd ) > 64 ) {
-				// eerst de rest al naar ronde twee zetten.
-				$ronde1 = count( $wedstrijd ) - ( 128 - count( $wedstrijd ) );  // 72 36games.
-				$ronde2 = count( $wedstrijd ) - $ronde1;  // 28 meteen naar ronde 2.
-				for ( $i = 1; $i < $ronde2; $i ++ ) {
-					// de eerste 28 stuks ronde 2
-					$game = new Wedstrijd();
-					$game->setRonde( 2 );
-					$game->setSpeler1ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ $i - 1 ] ] ) );
-					$game->setSpeler2ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ ++ $i - 1 ] ] ) );
-					$game->setToernooiID( $toernooi );
-					$em->persist( $game );
-					$em->flush();
-				}
-				for ( $i = $ronde2; $i < count( $wedstrijd ); $i ++ ) {
-					$game = new Wedstrijd();
-					$game->setRonde( 1 );
-					$game->setSpeler1ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ $i - 1 ] ] ) );
-					$game->setSpeler2ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ ++ $i - 1 ] ] ) );
-					$game->setToernooiID( $toernooi );
-					$em->persist( $game );
-					$em->flush();
-				}
-			} else {
-				return $this->render( 'default/index.html.twig', [
-					'controller_name' => 'DefaultController',
-				] );
 			}
-
+		} else {
+			return $this->render( 'default/index.html.twig', [
+				'controller_name' => 'DefaultController',
+			] );
 		}
 
 		return $this->render( 'wedstrijd/index.html.twig', [
@@ -132,41 +101,41 @@ class DefaultController extends AbstractController {
 	 */
 	public function makeRound( Toernooi $toernooi, $ronde ) {
 
-		$em = $this->getDoctrine()->getManager();
-
+		$em         = $this->getDoctrine()->getManager();
 		$toernooien = $em->getRepository( Toernooi::class )->findAll();
 
-		$repo        = $this->getDoctrine()->getRepository( Wedstrijd::class );
-		$query       = $repo->createQueryBuilder( 'p' )
-		                    ->where( 'p.ronde = :ronde' )
-		                    ->setParameter( 'ronde', number_format( $ronde - 1 ) )
-		                    ->getQuery();
-		$wedstrijden = $query->getResult();
+		// Haalt alle winnaars van vorige ronde op
+		$RAW_QUERY = 'select winnaar_id_id from wedstrijd where ronde = 1 and toernooi_id_id = :toernooi ;';
 
+		$statement = $em->getConnection()->prepare( $RAW_QUERY );
+		$statement->bindValue( 'toernooi', $toernooi->getId() );
+		$statement->execute();
+
+		// Alle winnaars van wedstijd in array
+		$wedstrijden = $statement->fetchAll();
+
+		// Stopt spelers in Array
 		$wedstrijd = [];
 		foreach ( $wedstrijden as $speler ) {
-			array_push( $wedstrijd, $speler->getWinnaarId() );
+			array_push( $wedstrijd, $speler );
 		}
 
 		if ( shuffle( $wedstrijd ) ) {
-			if ( count( $wedstrijd ) == 64 ) {
 
-				// genoeg spelers.
-				for ( $i = 1; $i < count( $wedstrijd ); $i ++ ) {
-					$game = new Wedstrijd();
-					$game->setRonde( 2 );
-					$game->setSpeler1ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ $i - 1 ] ] ) );
-					$game->setSpeler2ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ ++ $i - 1 ] ] ) );
-					$game->setToernooiID( $toernooi );
-					$em->persist( $game );
-					$em->flush();
-				}
-
-			} else {
-				return $this->redirectToRoute( 'default' );
+			// Nieuwe game maken
+			for ( $i = 1; $i < count( $wedstrijd ); $i ++ ) {
+				$game = new Wedstrijd();
+				$game->setRonde( 2 );
+				$game->setSpeler1ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ $i - 1 ] ] ) );
+				$game->setSpeler2ID( $em->getRepository( Speler::class )->findOneBy( [ 'id' => $wedstrijd[ ++ $i - 1 ] ] ) );
+				$game->setToernooiID( $toernooi );
+				$em->persist( $game );
+				$em->flush();
 			}
-
+		} else {
+			return $this->redirectToRoute( 'default' );
 		}
+
 
 		return $this->render( 'default/index.html.twig', [
 			'wedstrijd'  => $wedstrijden,
